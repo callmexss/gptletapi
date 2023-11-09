374d883d682bd5b493b1c8d20fa8af347d55ebe5
callmexss <callmexss@126.com>
2023-11-09 21:12:05 +0800

    chore: seperate settings.

---
+++ gptletapi/settings/prod.py
@@ -0,0 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+
+from .base import *  # noqa
+
+
+load_dotenv()
+
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
