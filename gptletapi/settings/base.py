4992c3f02ca1ad121567ffa46f3b219992e8d7b2
Yanyue <callmexss@126.com>
2023-11-26 07:37:28 +0000

    chore: remove whitenoise.

--- gptletapi/settings/base.py
+++ gptletapi/settings/base.py
@@ -129,12 +129,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
