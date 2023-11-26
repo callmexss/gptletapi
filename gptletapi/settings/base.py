a110fc6ba0df76196c617a0403a548835b7e3e7e
Yanyue <callmexss@126.com>
2023-11-26 08:25:50 +0000

    chore: use previous log setting.

--- gptletapi/settings/base.py
+++ gptletapi/settings/base.py
@@ -142,3 +142,22 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "debug.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
