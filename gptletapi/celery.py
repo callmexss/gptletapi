994fb38b203cbddc90baf3a1b43828896646a677
callmexss <callmexss@126.com>
2023-12-11 19:53:18 +0800

    fix: let django choose settings.

--- gptletapi/celery.py
+++ gptletapi/celery.py
@@ -1,11 +1,8 @@
 from __future__ import absolute_import, unicode_literals
-import os
 from celery import Celery
 from celery.schedules import crontab
 from django.conf import settings
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gptletapi.settings.base')
 
 app = Celery('gptletapi')
 app.config_from_object('django.conf:settings', namespace='CELERY')
